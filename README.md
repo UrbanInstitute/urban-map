c60563026080bc965f041d826675269e1217a23b
hrecht <hrecht@users.noreply.github.com>
2015-05-14 15:30:17 -0400

    Non-responsive map in Urban style

--- README.md
+++ README.md
@@ -6,9 +6,14 @@ d3 template for county and state level maps using Urban's style
 
 ##Features to add
 * Title
-* Tooltips
+* Tooltips - move to be where the pointer points (top left of poiner)
 * Legend
 * Predefined color ramps
 * Source info
 * (Eventually) optional embed button
-* Move geojson into script eventually 
+* Roll over legend to highlight bin in map
+* A feature that would be good to have is a "redacted" or "ignored flag" --> basically a way of saying "if Column X in the csv meets some condition, add a class to the path and grey it out, plus allow for a custom tooltip"
+
+##To do
+* Stop state paths from hover highlighting
+* Set no data color (allow choice or bake in?)
--- js/plain-map.js
+++ js/plain-map.js
@@ -1,98 +1,130 @@
-var width = 800,
-    height = width / 1.7;
+//Non-responsive but Urban style - moving variables that user will define outside the map function
 
-var margin = {
-    top: 30,
-    right: 10,
-    bottom: 10,
-    left: 10
-};
+var data_url = "data/schoolpoverty.csv";
+var json_url = "data/us.json";
+var names_url = "data/countynames.csv";
+var colors = ["#b0d5f1", "#82c4e9", "#0096d2", "#00578b", "#000"];
+var breaks = [0.2, 0.4, 0.6, 0.8];
+var legend_breaks = [0.2, 0.4, 0.6, 0.8, 1.0];
+var legend_left = 0;
+var formatter = d3.format("%");
 
-var svg = d3.select("#map").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var colors = ["#b0d5f1", "#82c4e9", "#0096d2", "#00578b", "#000"]
+function plainmap() {
 
-var color = d3.scale.threshold()
-    .domain([0.2, 0.4, 0.6, 0.8])
-    .range(colors);
+    var margin = {
+        top: 30,
+        right: 10,
+        bottom: 10,
+        left: 10
+    };
 
-var breaks = [0.20, 0.40, 0.60, 0.80, 1.00];
-var formatter = d3.format("%")
+    var width = 800,
+        height = width / 1.7;
 
-var legend = svg.selectAll("g.legend")
-    .data(breaks)
-    .enter().append("g")
-    .attr("class", "legend");
+    var svg = d3.select("#map").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var lp_w = 3 * width / 5,
-    ls_w = 40,
-    ls_h = 18;
+    var color = d3.scale.threshold()
+        .domain(breaks)
+        .range(colors);
 
-svg.append("text")
-    .text(formatter(0))
-    .attr("x", lp_w - 5)
-    .attr("y", 15)
-    .attr("class", "legend");
+    var legend = svg.selectAll("g.legend")
+        .data(legend_breaks)
+        .enter().append("g")
+        .attr("class", "legend");
 
-legend.append("rect")
-    .attr("x", function (d, i) {
-        return (i * ls_w) + lp_w;
-    })
-    .attr("y", 20)
-    .attr("width", ls_w)
-    .attr("height", ls_h)
-    .attr("z-index", 10)
-    .style("fill", function (d, i) {
-        return colors[i];
-    })
+    var lp_w = 3 * width / 5,
+        ls_w = 40,
+        ls_h = 18;
 
-legend.append("text")
-    .attr("x", function (d, i) {
-        return (i * ls_w) + lp_w + ls_w - 15;
-    })
-    .attr("y", 15)
-    .text(function (d, i) {
-        return formatter(d);
-    });
+    svg.append("text")
+        .text(formatter(legend_left))
+        .attr("x", lp_w - 5)
+        .attr("y", 15)
+        .attr("class", "legend");
 
-var value = d3.map();
+    legend.append("rect")
+        .attr("x", function (d, i) {
+            return (i * ls_w) + lp_w;
+        })
+        .attr("y", 20)
+        .attr("width", ls_w)
+        .attr("height", ls_h)
+        .attr("z-index", 10)
+        .style("fill", function (d, i) {
+            return colors[i];
+        })
 
-var projection = d3.geo.albersUsa()
-    .scale(width * 1.2)
-    .translate([width / 2, height / 2]);
-
-var path = d3.geo.path()
-    .projection(projection);
-
-queue()
-    .defer(d3.json, "data/us.json")
-    .defer(d3.csv, "data/schoolpoverty.csv")
-    .await(ready);
-
-function ready(error, us, matchtypes) {
-    var value = {};
-    matchtypes.forEach(function (d) {
-        value[d.id] = +d.PctPoorinPoorSchools;
-    });
-
-    svg.append("g")
-        .attr("class", "counties")
-        .selectAll("path")
-        .data(topojson.feature(us, us.objects.counties).features)
-        .enter().append("path")
-        .attr("d", path)
-        .style("fill", function (d) {
-            return color(value[d.id]);
+    legend.append("text")
+        .attr("x", function (d, i) {
+            return (i * ls_w) + lp_w + ls_w - 15;
+        })
+        .attr("y", 15)
+        .text(function (d, i) {
+            return formatter(d);
         });
 
-    svg.append("path")
-        .datum(topojson.mesh(us, us.objects.states))
-        .attr("class", "states")
-        .attr("d", path);
-}
+    var value = d3.map();
 
-d3.select(self.frameElement).style("height", height + "px");
+    var projection = d3.geo.albersUsa()
+        .scale(width * 1.2)
+        .translate([width / 2, height / 2]);
+
+    var path = d3.geo.path()
+        .projection(projection);
+
+    var pathst = d3.geo.path()
+        .projection(projection);
+
+    queue()
+        .defer(d3.json, json_url)
+        .defer(d3.csv, data_url)
+        .defer(d3.csv, names_url)
+        .await(ready);
+
+    function ready(error, us, data, names) {
+        var value = {};
+        var countyname = {};
+        data.forEach(function (d) {
+            value[d.id] = +d.PctPoorinPoorSchools;
+        });
+        names.forEach(function (d) {
+            countyname[d.id] = d.name;
+        });
+
+        svg.append("g")
+            .attr("class", "counties")
+            .selectAll("path")
+            .data(topojson.feature(us, us.objects.counties).features)
+            .enter().append("path")
+            .attr("d", path)
+            .style("fill", function (d) {
+                return color(value[d.id]);
+            })
+            .call(d3.helper.tooltip(
+                function (d, i) {
+                    return countyname[d.id] + "</br>" + formatter(value[d.id]);
+                }
+            ));;
+
+        svg.append("g")
+            .attr("class", "states")
+            .selectAll("pathst")
+            .data(topojson.feature(us, us.objects.states).features)
+            .enter().append("path")
+            .attr("d", pathst)
+            .style("fill", function (d) {
+                return color(value[d.id]);
+            });
+
+        //svg.append("path")
+        //    .datum(topojson.mesh(us, us.objects.states))
+        //   .attr("class", "states")
+        //    .attr("d", pathst);
+    }
+}
+plainmap();
